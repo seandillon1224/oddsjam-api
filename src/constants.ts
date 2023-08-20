7e7a4f6cb821428fd74e137122c9836dacc39bf8
Jordan Jones <21jrjones21@gmail.com>
2023-08-20 11:44:45 -0600

    Update constants.ts
    Odds Jam base url changed from /feed to /v2

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 type apiUrlTypes = 'games' | 'leagues' | 'markets' | 'game-odds' | 'futures' | 'future-odds' | 'scores'
 
-const API_ROOT_ENDPOINT = "https://api-external.oddsjam.com/api/feed/";
+const API_ROOT_ENDPOINT = "https://api-external.oddsjam.com/api/v2/";
 
 export const createURL = (apiKey: string) => (type: apiUrlTypes) => `${API_ROOT_ENDPOINT}${type}/?key=${apiKey}&`
